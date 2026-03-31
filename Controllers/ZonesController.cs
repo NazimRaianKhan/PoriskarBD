afd7e5223d55a3df80c673e47d76133bbfb0d73f
Md. Aribur Rahman Dhruvo <dhruvo.rahman0705@gmail.com>
2026-03-31 21:56:34 +0600

    Fixed naming conventions and messages

--- Controllers/ZonesController.cs
+++ Controllers/ZonesController.cs
@@ -20,19 +20,19 @@ namespace PoriskarBD.Controllers
 
         // GET: api/zones
         [HttpGet]
-        public async Task<IActionResult> GetAllZones()
+        public async Task<IActionResult> GetAll()
         {
-            var zones = await _zoneService.GetAllZonesAsync();
+            var zones = await _zoneService.GetAllAsync();
             return Ok(zones);
         }
 
         // GET: api/zones/{id}
         [HttpGet("{id}")]
-        public async Task<IActionResult> GetZoneById(int id)
+        public async Task<IActionResult> GetById(int id)
         {
-            var zone = await _zoneService.GetZoneByIdAsync(id);
-            if (zone == null)
-                return NotFound($"Zone with ID {id} not found.");
+            var zone = await _zoneService.GetByIdAsync(id);
+            if (zone == null) 
+                return NotFound(new { message = "Zone not found." });
             return Ok(zone);
         }
 
